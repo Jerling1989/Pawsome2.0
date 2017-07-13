81038cb785cd34a7cd261573edb12a7d49ca1dec
Jacob Erling <jacobs_macbook@Jacobs-MacBook-Pro.local>
2017-07-13 14:49:06 -0400

    adding on click function for comments

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,6 +8,6 @@ var path = require("path");
 
 module.exports = function(app){
 	app.get("/", function(req, res){
-		res.sendFile(path.join(__dirname, "../views/test.html"));
+		res.sendFile(path.join(__dirname, "../public/feed.html"));
 	})
 };
